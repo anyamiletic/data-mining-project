2cd25d64435830e4ad7753c1b821d9c8c8ec37d1
anyamiletic <anya.miletic@gmail.com>
2018-09-14 12:25:12 +0200

    proccesed duplicates, output basic info about data

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ def main():
 
 	print(df.head())
 	print(df.count())
+	print(df.dtypes)
 	
 	print(df.describe())
 	
-	for col in df.columns:
-	    print("count values in column " + col)
-	    print(df[col].value_counts(dropna = False))
+	# for col in df.columns:
+	#     print("count values in column " + col)
+	#     print(df[col].value_counts(dropna = False))
 
 	df[df['price'] < 100].plot.hexbin(x='price', y='points', gridsize=15)
 	#the hex plot shows clustering at the 20$-86pt mark
@@ -24,8 +25,21 @@ def main():
 	#shows that there are duplicate entries
 	df = df.drop_duplicates()
 	print(df.count())
-	#droping duplicates revealed about 30k duplicates
+	#droping duplicates revealed about 50k duplicates
 	#new count of dataframe is 97851
+
+	print(df.variety.describe())
+	#632 unique values for variety
+	ax = df['variety'].value_counts().head(7).sort_index().plot.bar(
+    figsize = (10,5),
+    fontsize = 14,
+    title = "Count of Variety"
+	)
+	plt.show()
+	#shows number of counts for first 7 varieties
+
+	df.to_csv("processed_data.csv")
+	
 
 if __name__ == '__main__':
 	main()
